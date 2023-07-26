e4fffb314a1d6995a50fb9294cc9dc7da895ec0a
exp-of-betrayal <glebisi@yandex.ru>
2023-07-26 10:31:46 +0400

    Update review.py

--- server/resources/review.py
+++ server/resources/review.py
@@ -56,7 +56,7 @@ class Reviews(Resource):
         booked_rooms_by_user = user.get_booked_rooms()
 
         if room_id not in booked_rooms_by_user:
-            abort(402, "You didn't book this room for review")
+            abort(400, "You didn't book this room for review")
 
         review = ReviewModel(
             user_id=user.id,
